739e39abe06edfda045cc88e540e84039bc3a150
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2020-12-02 04:24:24 -0400

    Update spotify_conexion.php

--- spotify_conexion.php
+++ spotify_conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","spotifyartist_db");
+$conex = mysqli_connect("de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ha1x4b8akcdtbw99","c36iryc13fin3bio","cjd7t6zch3t2lail");
 
 ?>
